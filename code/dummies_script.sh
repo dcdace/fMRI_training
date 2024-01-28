8c332b85b46113fb8d8317c1d74e1be93bb65c3c
Dace Apsvalka <dace.apshvalka@gmail.com>
2024-01-28 15:05:38 +0000

    added output comments

--- code/dummies_script.sh
+++ code/dummies_script.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
+# This script removes the first two volumes of a 4D nifti file  (dummies)
+# It takes one argument: the path to the 4D nifti file
+# It saves the modified file in the same path
+# It uses fslroi to remove the dummies
+#
+# Usage: ./dummies_script.sh <path_to_4D_nifti_file>
+
 file=$1
 n_dummies=2
 
+echo "Removing $n_dummies dummies from $file"
+echo "Output will be saved in the same file"
+
+echo "Loading FSL"
 module load fsl/6.0.1
 
-# Temporary output file
 temp_file="${file%.nii.gz}_temp.nii.gz"
+echo "Creating temporary file $temp_file"
 
-# Run fslroi with a temporary output file
+echo "Running fslroi with a temporary output file"
 fslroi "$file" "$temp_file" $n_dummies -1
 
-# Replace the original file with the modified file
+echo "Replacing the original file with the modified file"
 mv "$temp_file" "$file"
+
+echo "Done!"
